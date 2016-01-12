07270a8c9fd7cabe3847cb02ab2daaf8ea536d94
Emmanuel Genard <edgenard@gmail.com>
2016-01-12 01:24:04 +0000

    Finish player prompt

--- basketball.js
+++ basketball.js
@@ -1,4 +1,6 @@
 var promptForPlayerName = function (playerNumber) {
     var name = prompt(playerNumber + " what is your name?");
+    if(name.length > 15) alert("Wow that's a long name");
+    return name;
 };
 
